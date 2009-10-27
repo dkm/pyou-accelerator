759275693db5a783eb451d3bee1bf14cd8a3f2b8
Marc Poulhiès <dkm@kataplop.net>
2009-10-27 14:23:07 +0100

    bug fix. offset in array

--- sanatize.py
+++ sanatize.py
@@ -8,6 +8,6 @@ for l in f.xreadlines():
     d = l.split()
     dt = datetime.datetime.strptime(d[0], "%Y-%m-%dT%H:%M:%S.%f")
     ms = dt.minute*60*1000*1000 + dt.second * 1000 * 1000 + dt.microsecond
-    norm = math.sqrt(float(d[2])**2 + float(d[3])**2 + float(d[4])**2)
+    norm = math.sqrt(float(d[1])**2 + float(d[2])**2 + float(d[3])**2)
     r = [d[0]] + [str(ms)] + d[1:4] + [str(norm)]
     print " ".join(r)
